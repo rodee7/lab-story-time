bdc19bbcea8e4296073800b92aff5bde6a746228
“rodee7” <deepugovindan@gmail.com>
2021-10-17 15:33:12 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -2,15 +2,34 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(address, distanceFromTown, hasNeighbours)
+{
+    var datatypes  = typeof(address) + typeof(distanceFromTown) + typeof(hasNeighbours)
+    return datatypes
+}
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily)
+{
+    if(typeof(parents) == 'string' && typeof(noOfSiblings) == 'number' && typeof(isNuclearFamily) == 'boolean')
+    return true
+    else 
+    return false
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber) 
+{
+  if (isNaN(ageInText) == true)
+    return ageInText;
+  else 
+    return ageInNumber;
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -18,10 +37,33 @@
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel)
+{
+    if(isNaN(totalNoOfSweets))
+    return "No sweets for Karen's friend"
+    else if( totalNoOfSweets == 0)
+    return 0
+    else
+    {
+        totalNoOfSweets = totalNoOfSweets - sweetsConsumedByKaren - (sweetsConsumedInNMeters * metersToTravel)
+        if(totalNoOfSweets == 0)
+        return 0
+        else
+        return totalNoOfSweets
+    }
+}
 
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelsius(fahrenheit)
+{
+    let temperatureInCelcius = (5/9)*(fahrenheit-32)
+    if(typeof(fahrenheit) != 'number')
+    return 'Technical Error!'
+    else
+    return temperatureInCelcius
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -30,6 +72,18 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+function aDifficultChoice(choice)
+{
+if(choice==1)
+    return "Take her daughter to a doctor"
+else if(choice==-1)
+    return "Break down and give up all hope"
+else if(choice=='I give up')
+    return 'Refused to do anything for Karen'
+else
+    return "Wasn't able to decide"
+}
+
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
